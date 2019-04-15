be60d867a7a698bf03ed1b4d2b533d165b1c9eb5
daniel <daniel.zhang@ipiaoniu.com>
2019-04-16 00:31:15 +0800

    deepcopy design

---
+++ src/main/java/top/dannystone/deepcopier/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: daniel
+ * @creed: focus on the domain !
+ * @Time: 2019/4/15 11:17 PM
+ */
+package top.dannystone.deepcopier;
---
+++ src/main/java/top/dannystone/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: daniel
+ * @creed: focus on the domain !
+ * @Time: 2019/4/15 11:16 PM
+ */
+package top.dannystone;
