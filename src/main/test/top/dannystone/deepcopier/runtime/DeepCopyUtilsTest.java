45a1512bcbdc6febf3b7e919bfa7ae512e001845
daniel <daniel.zhang@ipiaoniu.com>
2019-05-22 00:26:04 +0800

    测试添加注释

--- src/main/test/top/dannystone/deepcopier/runtime/DeepCopyUtilsTest.java
+++ src/main/test/top/dannystone/deepcopier/runtime/DeepCopyUtilsTest.java
@@ -19,14 +19,21 @@ public class DeepCopyUtilsTest extends TestCase {
     @Test
     public void testDeepCopy(){
         Student student = new Student();
-        student.setAce(true);
         student.setAge(12);
         student.setName("daniel");
+        student.setAce(true);
+
         ClassRoom classroom = new ClassRoom();
         classroom.setClassName("classRoom1");
         student.setClassRoom(classroom);
-        student.setAce(true);
         Student studentCopy = DeepCopyUtils.deepCopy(student);
+
+        //测试继承属性
+        assertTrue(student.getAge()==studentCopy.getAge());
+        assertTrue(student.isAce()==studentCopy.isAce());
+        assertTrue(student.getName().equals(studentCopy.getName()));
+
+        //测试深复制
         assertTrue((studentCopy.getClassRoom()!= student.getClassRoom()&& studentCopy.getClassRoom().getClassName().equals(student.getClassRoom().getClassName())));
 
 
