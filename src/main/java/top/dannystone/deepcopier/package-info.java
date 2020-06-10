d7ecfcdfdd4be912165334da8c4550ff2a8c8069
daniel <daniel.zhang@ipiaoniu.com>
2020-06-10 14:40:02 +0800

    upgrade deepcopy to module

--- src/main/java/top/dannystone/deepcopier/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * Description:
- *
- * @author: daniel
- * @creed: focus on the domain !
- * @Time: 2019/4/15 11:17 PM
- */
-package top.dannystone.deepcopier;
--- src/main/java/top/dannystone/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * Description:
- *
- * @author: daniel
- * @creed: focus on the domain !
- * @Time: 2019/4/15 11:16 PM
- */
-package top.dannystone;
